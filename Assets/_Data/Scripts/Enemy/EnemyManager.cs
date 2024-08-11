4b91520c9b3f52455e3c176ccf1458cf25decbe4
Sai C4 <sai@saigame.studio>
2024-08-11 11:47:50 +0700

    e15

--- Assets/_Data/Scripts/Enemy/EnemyManager.cs
+++ Assets/_Data/Scripts/Enemy/EnemyManager.cs
@@ -5,16 +5,61 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     List<Enemy> enemies = new List<Enemy>();
+    Enemy smallestEnemey;
+    Enemy biggestEnemey;
 
     private void Awake()
     {
         this.LoadEnemeies();
     }
 
+    void Start()
+    {
+        this.LoadSmallestEnemy();
+        this.LoadBiggestEnemy();
+    }
+
+    protected virtual void LoadSmallestEnemy()
+    {
+        Enemy fistEnemey = this.enemies[0];
+        float smallestWeight = fistEnemey.GetMaxWeight();
+
+        foreach (Enemy enemy in this.enemies)
+        {
+            float enemyWeight = enemy.GetWeight();
+            if (enemyWeight < smallestWeight)
+            {
+                smallestWeight = enemyWeight;
+                this.smallestEnemey = enemy;
+            }
+
+            //Debug.Log(enemy.GetObjName() + " " + enemy.GetWeight());
+        }
+    }
+
+    protected virtual void LoadBiggestEnemy()
+    {
+        Enemy fistEnemey = this.enemies[0];
+        float biggestWeight = fistEnemey.GetMinWeight();
+
+        foreach (Enemy enemy in this.enemies)
+        {
+            float enemyWeight = enemy.GetWeight();
+            if (enemyWeight > biggestWeight)
+            {
+                biggestWeight = enemyWeight;
+                this.biggestEnemey = enemy;
+            }
+
+            Debug.Log(enemy.GetObjName() + " " + enemy.GetWeight());
+        }
+    }
+
+
     protected virtual void LoadEnemeies()
     {
 
-        foreach(Transform childObj in transform)
+        foreach (Transform childObj in transform)
         {
             Enemy enemy = childObj.GetComponent<Enemy>();
             if (enemy == null) continue;
