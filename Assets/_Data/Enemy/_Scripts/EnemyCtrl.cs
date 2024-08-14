0b3a5ef66b349e712e2851dcb842abdb49933855
Sai C4 <sai@saigame.studio>
2024-08-14 18:11:58 +0700

    e23

--- Assets/_Data/Enemy/_Scripts/EnemyCtrl.cs
+++ Assets/_Data/Enemy/_Scripts/EnemyCtrl.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class EnemyCtrl : SaiMonoBehaviour
 {
     [SerializeField] protected NavMeshAgent agent;
+    public NavMeshAgent Agent => agent;
 
     protected override void LoadComponents()
     {
--- Assets/_Data/Scripts/Enemy/EnemyMoving.cs
+++ Assets/_Data/Scripts/Enemy/EnemyMoving.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyMoving : MonoBehaviour
+public class EnemyMoving : SaiMonoBehaviour
 {
     public GameObject target;
-    public NavMeshAgent agent;
+    [SerializeField] protected EnemyCtrl enemyCtrl;
 
-    void Start()
-    {
-        
-    }
 
     void FixedUpdate()
     {
-        agent.SetDestination(target.transform.position);
+        this.Moving();
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadEnemyCtrl();
+        this.LoadTarget();
+    }
+
+
+    protected virtual void LoadEnemyCtrl()
+    {
+        if (this.enemyCtrl != null) return;
+        this.enemyCtrl = transform.parent.GetComponent<EnemyCtrl>();
+        Debug.Log(transform.name + ": LoadEnemyCtrl", gameObject);
+    }
+
+    protected virtual void LoadTarget()
+    {
+        if (this.target != null) return;
+        this.target = GameObject.Find("TargetMoving");
+        Debug.Log(transform.name + ": LoadTarget", gameObject);
+    }
+
+    protected virtual void Moving()
+    {
+        this.enemyCtrl.Agent.SetDestination(target.transform.position);
     }
 }
