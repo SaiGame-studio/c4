0657c0524b7d53103d31739b1b464f59ecd56137
Sai C4 <sai@saigame.studio>
2024-08-18 22:33:18 +0700

    e26

---
+++ Assets/_Data/Paths/Path.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : SaiMonoBehaviour
+{
+    [SerializeField] protected List<Point> points;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadPoints();
+    }
+
+    public virtual void LoadPoints()
+    {
+        if (this.points.Count > 0) return;
+        foreach (Transform child in transform)
+        {
+            Point point = child.GetComponent<Point>();
+            this.points.Add(point);
+        }
+        Debug.Log(transform.name + ": LoadPoints", gameObject);
+    }
+}
---
+++ Assets/_Data/Paths/PathsManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathsManager : SaiMonoBehaviour
+{
+    [SerializeField] protected List<Path> paths = new();
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadPaths();
+    }
+
+    protected virtual void LoadPaths()
+    {
+        if (this.paths.Count > 0) return;
+        foreach (Transform child in transform)
+        {
+            Path path = child.GetComponent<Path>();
+            path.LoadPoints();
+            this.paths.Add(path);
+        }
+        Debug.Log(transform.name + ": LoadPaths", gameObject);
+    }
+}
