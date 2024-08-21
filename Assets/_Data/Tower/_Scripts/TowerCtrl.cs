ec43f9ac71caeccd9de62ec566947b2ba0474c7b
Sai C4 <sai@saigame.studio>
2024-08-22 03:04:08 +0700

    e29b

---
+++ Assets/_Data/Tower/_Scripts/TowerCtrl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCtrl : SaiMonoBehaviour
+{
+    [SerializeField] protected Transform model;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadModel();
+    }
+
+    protected virtual void LoadModel()
+    {
+        if (this.model != null) return;
+        this.model = transform.Find("Model");
+        Debug.Log(transform.name + ": LoadModel", gameObject);
+    }
+
+}
---
+++ Assets/_Data/Tower/_Scripts/TowerTargetable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTargetable : SaiMonoBehaviour
+{
+    [SerializeField] protected SphereCollider sphereCollider;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadSphereCollider();
+    }
+
+    protected virtual void LoadSphereCollider()
+    {
+        if (this.sphereCollider != null) return;
+        this.sphereCollider = GetComponent<SphereCollider>();
+        this.sphereCollider.radius = 0.5f;
+        this.sphereCollider.isTrigger = true;
+        Debug.Log(transform.name + ": LoadSphereCollider", gameObject);
+    }
+
+}
