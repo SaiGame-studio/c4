424888fc1980566661c75af1935ed9fb1252a9f2
Sai C4 <sai@saigame.studio>
2024-08-24 17:04:49 +0700

    e31

--- Assets/_Data/Tower/_Scripts/TowerTargetable.cs
+++ Assets/_Data/Tower/_Scripts/TowerTargetable.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
 public class TowerTargetable : SaiMonoBehaviour
 {
     [SerializeField] protected SphereCollider sphereCollider;
