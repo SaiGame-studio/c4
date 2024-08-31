b7430f70dcb9e4f1b8609afbfaba3f5a851f3aed
Sai C4 <sai@saigame.studio>
2024-09-01 02:12:35 +0700

    e37

--- Assets/_Data/Spawner/BulletSpawner.cs
+++ Assets/_Data/Spawner/BulletSpawner.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class BulletSpawner : Spawner
 {
-    
+    public virtual Bullet Spawn(Bullet bulletPrefab)
+    {
+        Bullet newObject = Instantiate(bulletPrefab);
+        return newObject;
+    }
+
+    public virtual Bullet Spawn(Bullet buletPrefab, Vector3 postion)
+    {
+        Bullet newBullet = this.Spawn(buletPrefab);
+        newBullet.transform.position = postion;
+        return newBullet;
+    }
 }
