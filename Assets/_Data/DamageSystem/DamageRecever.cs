d4e0fdcfd677c6ba73ed53be3541650288d6e493
Sai C4 <sai@saigame.studio>
2024-09-02 02:56:07 +0700

    e45

--- Assets/_Data/DamageSystem/DamageRecever.cs
+++ Assets/_Data/DamageSystem/DamageRecever.cs
@@ -7,11 +7,14 @@ public abstract class DamageRecever : SaiMonoBehaviour
     protected int maxHP = 10;
     protected int currentHP = 10;
     protected bool isDead = false;
+    [SerializeField] protected bool isImmotal = false;
 
     public virtual int Deduct(int hp)
     {
-        this.currentHP -= hp;
-        this.IsDead();
+        if(!this.isImmotal) this.currentHP -= hp;
+
+        if (this.IsDead()) this.OnDead();
+        else this.OnHurt();
 
         if (this.currentHP < 0) this.currentHP = 0;
         return currentHP;
@@ -20,5 +23,15 @@ public abstract class DamageRecever : SaiMonoBehaviour
     protected virtual bool IsDead()
     {
         return this.isDead = this.currentHP <= 0;
+    }
+
+    protected virtual void OnDead()
+    {
+        //For override
+    }
+
+    protected virtual void OnHurt()
+    {
+        //For override
     }
 }
