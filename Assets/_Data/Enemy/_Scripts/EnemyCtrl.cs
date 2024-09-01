04c12e0483f5e4b7abf98f118d4fbc747e9b47f6
Sai C4 <sai@saigame.studio>
2024-09-02 03:35:39 +0700

    e46

--- Assets/_Data/Enemy/_Scripts/EnemyCtrl.cs
+++ Assets/_Data/Enemy/_Scripts/EnemyCtrl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyCtrl : SaiMonoBehaviour
+public abstract class EnemyCtrl : PoolObj
 {
     [SerializeField] protected Transform model;
 
@@ -42,7 +42,6 @@ public class EnemyCtrl : SaiMonoBehaviour
         this.model.localPosition = new Vector3(0f, 0f, 0f);
         Debug.Log(transform.name + ": LoadModel", gameObject);
     }
-
 
     protected virtual void LoadTowerTargetable()
     {
