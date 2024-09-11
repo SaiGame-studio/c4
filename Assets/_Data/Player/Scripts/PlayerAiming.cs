b7b877837d6b40e66e892f05c3af3e67c52fd668
Sai C4 <sai@saigame.studio>
2024-09-12 04:22:58 +0700

    coding

--- Assets/_Data/Player/Scripts/PlayerAiming.cs
+++ Assets/_Data/Player/Scripts/PlayerAiming.cs
@@ -22,9 +22,7 @@ public class PlayerAiming : PlayerAbstract
 
         CrosshairPointer crosshairPointer = this.playerCtrl.CrosshairPointer;
         this.playerCtrl.ThirdPersonController.RotateToPosition(crosshairPointer.transform.position);
-
         this.playerCtrl.ThirdPersonController.isSprinting = false;
-
         this.playerCtrl.AimingRig.weight = 1;
     }
 
