baf6a6c89b88451717616acf9b77c221a811ebf9
Sai C4 <sai@saigame.studio>
2024-09-17 06:40:32 +0700

    e63

--- Assets/_Data/Player/Skill/AttackLight.cs
+++ Assets/_Data/Player/Skill/AttackLight.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class AttackLight : AttackAbstract
 {
+    protected string effectName = "Fire1";
+
     protected override void Attacking()
     {
         if (!InputManager.Instance.IsAttackLight()) return;
-
         AttackPoint attackPoint = this.GetAttackPoint();
-
+        EffectCtrl effect = this.spawner.Spawn(this.GetEffect(), attackPoint.transform.position);
+        effect.gameObject.SetActive(true);
         Debug.Log("Light Attack");
+    }
+
+    protected virtual EffectCtrl GetEffect()
+    {
+        return this.prefabs.GetByName(this.effectName);
     }
 }
