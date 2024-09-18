68643de11771ea214bc2c040bdfab928b0f1bcd4
Sai C4 <sai@saigame.studio>
2024-09-19 05:22:26 +0700

    coding

--- Assets/_Data/Player/Skill/AttackHeavy.cs
+++ Assets/_Data/Player/Skill/AttackHeavy.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class AttackHeavy : AttackAbstract
 {
     protected string effectName = "Fire2";
+    protected float timer = 0;
+    protected float delay = 0.1f;
 
     protected override void Attacking()
     {
         if (!InputManager.Instance.IsAttackHeavy()) return;
+
+        this.timer += Time.deltaTime;
+        if (this.timer < this.delay) return;
+        this.timer = 0;
+
         AttackPoint attackPoint = this.GetAttackPoint();
         EffectCtrl effect = this.spawner.Spawn(this.GetEffect(), attackPoint.transform.position);
 
