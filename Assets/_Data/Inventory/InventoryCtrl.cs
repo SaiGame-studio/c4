a5cfc20323132e9a5afb88b79ea383798678dbfe
Sai C4 <sai@saigame.studio>
2024-09-19 05:02:43 +0700

    e65

---
+++ Assets/_Data/Inventory/InventoryCtrl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InventoryCtrl : SaiMonoBehaviour
+{
+    protected List<ItemInventory> items = new();
+
+    public abstract InvCodeName GetName();
+
+    public virtual void AddItem(ItemInventory item)
+    {
+        this.items.Add(item);
+    }
+}
