f405766f76cb737447d8b60add1d81e3efd82806
Sai C4 <sai@saigame.studio>
2024-09-24 17:09:12 +0700

    Coding

--- Assets/_Data/Inventory/ItemDrop/ItemPicker.cs
+++ Assets/_Data/Inventory/ItemDrop/ItemPicker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
 public class ItemPicker : SaiMonoBehaviour
 {
     [SerializeField] protected SphereCollider sphereCollider;
