1e89e0533aa82a02131768f3af46828156b167da
Sai C4 <sai@saigame.studio>
2024-09-24 16:39:16 +0700

    coding

---
+++ Assets/_Data/Inventory/ItemDrop/ItemPicker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPicker : SaiMonoBehaviour
+{
+    [SerializeField] protected SphereCollider sphereCollider;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadSphereCollider();
+    }
+
+    protected virtual void LoadSphereCollider()
+    {
+        if (this.sphereCollider != null) return;
+        this.sphereCollider = GetComponent<SphereCollider>();
+        this.sphereCollider.radius = 0.3f;
+        this.sphereCollider.isTrigger = true;
+        Debug.Log(transform.name + ": LoadSphereCollider", gameObject);
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name,other.gameObject);
+    }
+}
