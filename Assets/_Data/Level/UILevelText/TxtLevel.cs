0e08ee6387dcfeb27f0e0f1eca2dc68c3b1aca63
Sai C4 <sai@saigame.studio>
2024-10-03 02:57:06 +0700

    e83

--- Assets/_Data/Level/UILevelText/TxtLevel.cs
+++ Assets/_Data/Level/UILevelText/TxtLevel.cs
@@ -2,30 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TxtLevel : Text3DAbstact
+public abstract class TxtLevel : Text3DAbstact
 {
-    [SerializeField] protected TowerCtrl towerCtrl;
-
     protected virtual void FixedUpdate()
     {
         this.UpdateLevel();
     }
 
-    protected override void LoadComponents()
-    {
-        base.LoadComponents();
-        this.LoadTowerCtrl();
-    }
-
-    protected virtual void LoadTowerCtrl()
-    {
-        if (this.towerCtrl != null) return;
-        this.towerCtrl = GetComponentInParent<TowerCtrl>();
-        Debug.Log(transform.name + ": LoadTowerCtrl", gameObject);
-    }
-
     protected virtual void UpdateLevel()
     {
-        this.textPro.text = this.towerCtrl.Level.CurrentLevel.ToString();
+        this.textPro.text = this.GetLevel();
     }
+
+    protected abstract string GetLevel();
 }
