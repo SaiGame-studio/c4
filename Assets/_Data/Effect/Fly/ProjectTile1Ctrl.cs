31a2f94ffc2630e4598c4109fb51fbab49c1e35e
Sai C4 <sai@saigame.studio>
2024-10-05 01:22:09 +0700

    coding

---
+++ Assets/_Data/Effect/Fly/ProjectTile1Ctrl.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ProjectTile1Ctrl : EffectFlyAbtract
+{
+    public override string GetName()
+    {
+        return "ProjectTile1";
+    }
+}
