850ab611d149f909adce4086487e2b32748d9fa8
Sai C4 <sai@saigame.studio>
2024-10-20 23:31:46 +0700

    coding

---
+++ Assets/_Data/UI/Button/BtnMusicToggle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnMusicToggle : ButttonAbstract
+{
+    protected override void OnClick()
+    {
+        //SoundManager.Instance.ToggleMusic();
+    }
+}
